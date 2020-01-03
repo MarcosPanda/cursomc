da776409018630bb4407f17a912415c72b539a12
Marcos <marcosleandronascimento@hotmail.com>
2020-01-03 00:04:53 -0300

    Ajuste no endpoint /categorias/{id} Aula 17

--- src/main/java/com/marcos/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -20,12 +20,6 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		/*
-		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado, id: " + id + 
-					", Tipo; " + Categoria.class.getName());
-		}*/
-		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 	}
